dcdeca754df03169d31b4186a1e22045912794ae
Muhammad Talha <137063456+muheesjutt@users.noreply.github.com>
2025-07-21 15:13:32 +0500

    creating project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from './section/About'
 import Hero from './section/Hero'
 import Navbar from './section/Navbar'
+import Projects from './section/Projects'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero/> 
       <About/>
+      <Projects/>
     </main>
   )
 }
